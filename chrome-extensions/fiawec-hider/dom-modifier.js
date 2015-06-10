a1b373968ccf623403429f88d8dfeee27acd27d7
fraz3alpha <andrewtaylor2@gmail.com>
2015-06-10 19:31:32 +0100

    Implement iframe resizing

--- chrome-extensions/fiawec-hider/dom-modifier.js
+++ chrome-extensions/fiawec-hider/dom-modifier.js
@@ -1,6 +1,6 @@
 console.log("Removing annoying bits around the video")
 
-var nodes_to_remove = ['breakingnews', 'etatCourse', 'meteo', 'previsions', 'chronos', 'channels']
+var nodes_to_remove = ['breakingnews', 'etatCourse', 'meteo', 'previsions', 'chronos', 'channels', 'ddlCategorieChronos-menu']
 var nodes_to_remove_class = ['header', 'subcontent', 'logout', 'tudor', 'retour']
  
 var nodes = document.getElementsByTagName("div")
@@ -15,24 +15,41 @@ for (var i=0; i<nodes.length; i++) {
 // Remove styles
 $('link[rel=stylesheet]').prop('disabled',true);
 
-$("div").each(function (index) {
-  id = $(this).attr('id')
-  class_value = $(this).attr('class')
+function removeElement(index, e) {
+
+  id = $(e).attr('id');
+  class_value = $(e).attr('class')
 
   if (id) {
     if ($.inArray(id, nodes_to_remove) > -1) {
       console.log(index + ": " + id);
-      $(this).remove()
+      $(e).remove()
     }
   }
 
   if (class_value) {
     if ($.inArray(class_value, nodes_to_remove_class) > -1) {
       console.log(index + ": " + class_value);
-      $(this).remove()
+      $(e).remove()
     }
-  }
+ 
+ }
+}
+
+$("div").each(function (index) {
+  removeElement(index, this);
+});
+$("ul").each(function (index) {
+  removeElement(index, this);
 });
 
 console.log('Setting the background to black')
 document.body.style.backgroundColor="black"
+
+console.log('Making the video larger')
+
+$('#zoneVideo iframe').each(function (index) {
+  console.log(this);
+  $(this).attr('height', '700');
+  $(this).attr('width', '1400');
+});
